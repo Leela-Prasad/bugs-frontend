207e24eb81bbd00b4730887d33b32cff075ad90a
Leela-Prasad <leela.jagu@gmail.com>
2024-09-08 07:23:38 +0530

    Initial Setup with older versions of libraries

---
+++ src/components/Bugs.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { loadBugs, resolveBug, getUnresolvedBugs } from "../store/bugs";
+import { connect } from "react-redux";
+
+class Bugs extends Component {
+  
+  render() {
+    return (
+      <div>Bug</div>
+    );
+  }
+}
---
+++ src/components/BugsList.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadBugs, getUnresolvedBugs, resolveBug } from "../store/bugs";
+
+const BugsList = () => {
+  return (
+    <div>BugsList</div>
+  );
+};
+
+export default BugsList;

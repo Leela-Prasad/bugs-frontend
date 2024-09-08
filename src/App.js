71c4211136c525ba1e14e80094b0e5953699571a
Leela-Prasad <leela.jagu@gmail.com>
2024-09-08 15:53:05 +0530

    Display Bugs using Class based Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import configureStore from "./store/configureStore";
 import BugsList from "./components/BugsList";
 import { Provider } from "react-redux";
+import Bugs from "./components/Bugs";
 
 const store = configureStore();
 
 function App() {
   return (
     <Provider store={store}>
-      <BugsList />    
+      <Bugs />
+      {/* <BugsList />     */}
     </Provider>
   );
 }
--- src/components/Bugs.jsx
+++ src/components/Bugs.jsx
@@ -3,10 +3,26 @@ import { loadBugs, resolveBug, getUnresolvedBugs } from "../store/bugs";
 import { connect } from "react-redux";
 
 class Bugs extends Component {
-  
+  componentDidMount() {
+    this.props.loadBugs()
+  }
+
   render() {
-    return (
-      <div>Bug</div>
-    );
+    return <ul>
+      {this.props.bugs.map(bug => <li key={bug.id}>{bug.description}
+                          <button onClick={() => this.props.resolveBugs(bug.id)}>Resolve</button>
+                        </li>)}
+    </ul>
   }
 }
+
+const mapStateToProps = state => ({
+  bugs: getUnresolvedBugs(state)
+})
+
+const mapDispatchToProps = dispatch => ({
+  loadBugs: () => dispatch(loadBugs()),
+  resolveBugs: id => dispatch(resolveBug(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Bugs);

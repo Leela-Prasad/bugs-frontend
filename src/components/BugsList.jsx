f226ac4aa02f15b621af5dea73e9df2f732567e5
Leela-Prasad <leela.jagu@gmail.com>
2024-09-08 07:37:13 +0530

    loadBugs and render in UI

--- src/components/BugsList.jsx
+++ src/components/BugsList.jsx
@@ -3,8 +3,17 @@ import { useDispatch, useSelector } from "react-redux";
 import { loadBugs, getUnresolvedBugs, resolveBug } from "../store/bugs";
 
 const BugsList = () => {
+  const dispatch = useDispatch();
+  const bugs = useSelector(store => store.entities.bugs.list)
+  
+  useEffect(() => {
+    dispatch(loadBugs())
+  }, []);
+
   return (
-    <div>BugsList</div>
+    <div><ul>
+        {bugs.map(bug => <li key={bug.id}>{bug.description}</li>)}
+      </ul></div>
   );
 };
 

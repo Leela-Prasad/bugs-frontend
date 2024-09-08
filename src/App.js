baea84516ba5deab146a64376e38ec24573a12a4
Leela-Prasad <leela.jagu@gmail.com>
2024-09-08 07:30:47 +0530

    Configure Redux Store in React App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import configureStore from "./store/configureStore";
 import BugsList from "./components/BugsList";
+import { Provider } from "react-redux";
 
 const store = configureStore();
 
 function App() {
   return (
+    <Provider store={store}>
       <BugsList />    
+    </Provider>
   );
 }
 
